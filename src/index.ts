4200d0f692cf5c83d5e800f9e818c57468f8777d
Dennis Christensen <dennis@entropyfox.com>
2022-01-24 11:03:06 +0100

    Created cli starter

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import clear from "clear";
+import chalk from "chalk";
+import figlet from "figlet";
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('DNRN', { horizontalLayout: 'full' })
+  )
+);
